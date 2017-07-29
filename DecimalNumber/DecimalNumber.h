315b9c73670f298d8f8379a21103fc5ab3da6552
Michael Griebling <sam@c-inspirations.com>
2017-07-29 08:00:20 -0400

    Initial Commit

---
+++ DecimalNumber/DecimalNumber.h
@@ -0,0 +1,19 @@
+//
+//  DecimalNumber.h
+//  DecimalNumber
+//
+//  Created by Mike Griebling on 29 Jul 2017.
+//  Copyright © 2017 Computer Inspirations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DecimalNumber.
+FOUNDATION_EXPORT double DecimalNumberVersionNumber;
+
+//! Project version string for DecimalNumber.
+FOUNDATION_EXPORT const unsigned char DecimalNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DecimalNumber/PublicHeader.h>
+
+
